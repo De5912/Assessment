b0e07d6ac1b680ecb4b4493ae217cb76191179fa
Dev <dev.bharti@s.amity.edu>
2025-05-06 11:17:35 +0530

    Program to calculate 3 numbers

---
+++ Test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+	int a, b, c;
+	printf("Enter the Value of a, b and c:\n");
+	scanf("%d%d%d", &a, &b, &c);
+	sum = a + b + c;
+	printf("The sum of a, b and c is", &sum);
+	return 0;
+}
