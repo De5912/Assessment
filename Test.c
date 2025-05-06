047ad49bcb834aecf9f5b7ea916c997197751f44
Sizils5932 <dev.bharti@s.amity.edu>
2025-05-06 11:26:01 +0530

    Added Avg in program

--- Test.c
+++ Test.c
@@ -4,7 +4,7 @@ int main () {
 	printf("Enter the Value of a, b and c:\n");
 	scanf("%d%d%d", &a, &b, &c);
 	sum = a + b + c;
-	avg = sum/3
+	avg = sum/3;
 	printf("The sum of a, b and c is %d", &sum);
 	printf("The average is %d", &avg);
 	return 0;
