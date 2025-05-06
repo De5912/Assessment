b8e3fe435390ddcf660ece1f5d8bc9607b4f75fa
Dev <dev.bharti@s.amity.edu>
2025-05-06 11:20:42 +0530

    To calculate the sum of three numbers

--- Test.c
+++ Test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main () {
-	int a, b, c;
+	int a, b, c, sum;
 	printf("Enter the Value of a, b and c:\n");
 	scanf("%d%d%d", &a, &b, &c);
 	sum = a + b + c;
-	printf("The sum of a, b and c is", &sum);
+	printf("The sum of a, b and c is %d", &sum);
 	return 0;
 }
