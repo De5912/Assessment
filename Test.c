72ee6955cb0e0a7035b81309dbc50a3825f05642
Dev <dev.bharti@s.amity.edu>
2025-05-06 11:27:23 +0530

    Added Multiplication in the program

--- Test.c
+++ Test.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main () {
-	int a, b, c, sum;
+	int a, b, c, sum, avg, mul;
 	printf("Enter the Value of a, b and c:\n");
 	scanf("%d%d%d", &a, &b, &c);
 	sum = a + b + c;
+	avg = sum/3;
+	mul = a * b * c;
 	printf("The sum of a, b and c is %d", &sum);
+	printf("The avg is %d", &avg);
+	printf("The multiplication is %d", &mul);
 	return 0;
 }
